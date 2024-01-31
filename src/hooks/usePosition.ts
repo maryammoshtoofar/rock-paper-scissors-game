2bda29a2b18688722dfab162fe7d84429c6c9a51
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2024-01-31 16:17:46 +0330

    cleanup

--- src/hooks/usePosition.ts
+++ src/hooks/usePosition.ts
@@ -11,10 +11,14 @@ const usePosition = (order: 1 | 2 | 3 | 4 | 5) => {
         setPosition("translate-x-full translate-y-1/2");
         break;
       case 3:
-        setPosition("translate-y-44 sm:translate-y-56 translate-x-3/4");
+        setPosition(
+          "translate-y-44 max-[308px]:translate-y-36  sm:translate-y-56 translate-x-3/4 "
+        );
         break;
       case 4:
-        setPosition("translate-y-44 sm:translate-y-56 -translate-x-3/4");
+        setPosition(
+          "translate-y-44 max-[308px]:translate-y-36 sm:translate-y-56 -translate-x-3/4"
+        );
         break;
       case 5:
         setPosition("-translate-x-full translate-y-1/2");
